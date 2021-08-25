09643c944625edff3b07c485c0b51893bc333d96
Hwls_ek <nunnunan0321@naver.com>
2021-08-25 19:01:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/lets_game/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.lets_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myCard_first:Int
+
+
+
+    }
+}
